a06b391239c2e834e28e5bad2d4291fc81878022
PurpHunter <brodyf2008@gmail.com>
2026-02-04 21:52:07 -0500

    Delete AI Development (Final) directory

--- AI Development (Final)/federated_learning/export_model.py
+++
@@ -1,9 +0,0 @@
-import requests, joblib, numpy as np, os
-
-os.makedirs("central_server/models", exist_ok=True)
-
-m = requests.get("http://127.0.0.1:5000/global_model").json()
-joblib.dump(
-    {"coef": np.array(m["coef"]), "intercept": np.array(m["intercept"])},
-    "central_server/models/mental_health_classifier.joblib"
-)
